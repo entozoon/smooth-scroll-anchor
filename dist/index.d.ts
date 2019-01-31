aecdcf73c07afdb3ae14c65353c94cc1b897d26b
Michael Cook <entozoon@gmail.com>
2019-01-31 12:08:18 +0000

    Typin' it up

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+declare type Block = "start" | "center" | "end" | "nearest";
+export declare const smoothScroll: (e: Event, element: Element, block?: Block) => void;
+export declare const init: () => void;
+export {};
---
+++ dist/index.js
@@ -0,0 +1,36 @@
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// IE11/Edge scrollIntoView polyfill
+var smoothscroll = __importStar(require("smoothscroll-polyfill"));
+// const smoothscroll = require("smoothscroll-polyfill");
+smoothscroll.polyfill();
+exports.smoothScroll = function (e, element, block) {
+    if (block === void 0) { block = "center"; }
+    e.preventDefault();
+    // Native modern functionality with IE11 support in a basic way, via polyfill
+    console.log(block);
+    element.scrollIntoView({
+        behavior: "smooth",
+        block: block
+    });
+};
+// Loop over anchor links
+exports.init = function () {
+    var anchorLinks = document.querySelectorAll('[href^="#"]:not([href="#"]');
+    for (var i = 0; i < anchorLinks.length; i++) {
+        anchorLinks[i].addEventListener("click", function (e) {
+            // Grab the href and thence the targetted element
+            var targetSelector = e.target.getAttribute("href") || "", target = document.querySelector(targetSelector);
+            if (target) {
+                exports.smoothScroll(e, target);
+            }
+        });
+    }
+};
